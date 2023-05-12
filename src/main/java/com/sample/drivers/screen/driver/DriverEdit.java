feb2c34528479319416582b9fc89877fdf1cd34c
Urjadovaa <urjadovaa@gmail.com>
2023-05-12 20:11:02 +0300

    ДЗ полностью готово!

--- src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
+++ src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
@@ -1,6 +1,11 @@
 package com.sample.drivers.screen.driver;
 
+import com.sample.drivers.entity.Document;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.Component;
 import io.jmix.ui.component.Image;
+import io.jmix.ui.component.LinkButton;
+import io.jmix.ui.download.Downloader;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Driver;
@@ -13,6 +18,10 @@ public class DriverEdit extends StandardEditor<Driver> {
 
     @Autowired
     private Image<byte[]> driverPhoto;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private Downloader downloader;
 
     // (*) (опционально) скрывать компонент, если фотография водителя еще не загружена.
 
@@ -24,6 +33,17 @@ public class DriverEdit extends StandardEditor<Driver> {
     @Subscribe(id = "driverDc", target = Target.DATA_CONTAINER)
     public void onDriverDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Driver> event) {
         driverPhoto.setVisible("photo".equals(event.getProperty()) && event.getValue() != null);
+    }
+
+    @Install(to = "documentsTable.file", subject = "columnGenerator")
+    private Component documentsTableFileColumnGenerator(Document document) {
+        if (document.getFile() == null) return null;
+        LinkButton linkButton = uiComponents.create(LinkButton.class);
+        linkButton.setCaption(document.getFile().getFileName());
+        linkButton.addClickListener(clickEvent -> {
+            downloader.download(document.getFile());
+        });
+        return linkButton;
     }
 
 }
